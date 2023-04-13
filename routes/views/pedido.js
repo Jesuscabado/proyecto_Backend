0e635c53040183f1d3802e540c8e7399170942ec
Jesuscabado <jesuscabado@hotmail.com>
2023-04-13 19:34:43 +0200

    añadidas mas rutas y pedido.pug

--- routes/views/pedido.js
+++ routes/views/pedido.js
@@ -1,4 +1,4 @@
-/* import { Router } from "express";
+import { Router } from "express";
 import { isAuthorized,isAdmin } from "../../middlewares/auth.js";
 import pedidoController from "../controllers/Pedido/pedidoViewController.js";
 
@@ -9,11 +9,15 @@ router.get("/", isAuthorized,isAdmin , (req, res) => {
 });
 //ruta para agregar producto al carrito
 
-/* router.get("/carrito",isAuthorized, (req, res) => {
+ router.get("/carrito",isAuthorized, (req, res) => {
     pedidoController.createPedido(req, res);
 });
+
+router.get("/carrito/:id", isAuthorized, (req, res) => {
+    pedidoController.addProducto(req, res);
+});
  
-router.get */
+router.get 
 
-
+export default router;
 
